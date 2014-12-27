e6bfc9fcf84ddeef571684c752e51914e8e20f21
Grégory Bataille <gbataille.dev@gmail.com>
2014-12-27 12:48:02 +0100

    date 2015

--- js/darx.js
+++ js/darx.js
@@ -46,17 +46,32 @@ function postLoadMagic() {
   $(".magic-calendar").responsiveCalendar({
     time: $.datepicker.formatDate('yy-mm', new Date()),
     events: {
-      "2014-10-25": {},
-      "2014-11-08": {}, 
-      "2014-11-22": {}, 
-        "2014-12-13": {
-          "number" : 1,
-          "badgeClass":"badge-warning", 
-          "dayEvents": [
-          {"name":"journée continue",
-            "hour":"09:00"}
-          ]
-        }
+      "2015-01-02": {},
+      "2015-01-09": {},
+      "2015-01-16": {},
+      "2015-01-23": {},
+      "2015-01-30": {},
+      "2015-02-06": {},
+      "2015-02-13": {},
+      "2015-02-20": {},
+      "2015-02-27": {},
+      "2015-03-06": {},
+      "2015-03-13": {},
+      "2015-03-20": {},
+      "2015-03-27": {},
+      "2015-04-03": {},
+      "2015-04-10": {},
+      "2015-04-17": {},
+      "2015-04-24": {},
+      "2015-05-01": {},
+      "2015-05-08": {},
+      "2015-05-15": {},
+      "2015-05-22": {},
+      "2015-05-29": {},
+      "2015-06-05": {},
+      "2015-06-12": {},
+      "2015-06-19": {},
+      "2015-06-26": {}
     }
   });
 }
@@ -79,17 +94,64 @@ function postLoadHome() {
   $(".responsive-calendar").responsiveCalendar({
     time: $.datepicker.formatDate('yy-mm', new Date()),
     events: {
-      "2014-10-25": {},
-      "2014-11-08": {}, 
-      "2014-11-22": {}, 
-        "2014-12-13": {
-          "number" : 1,
-          "badgeClass":"badge-warning", 
-          "dayEvents": [
-          {"name":"journée continue",
-            "hour":"09:00"}
-          ]
-        }
+      "2014-12-13": {
+        "number" : 1,
+        "badgeClass":"badge-warning", 
+        "dayEvents": [
+        {"name":"journée continue",
+          "hour":"09:00"}
+        ]
+      },
+      "2015-01-10": {},
+      "2015-01-24": {
+        "number" : 1,
+        "badgeClass":"badge-warning", 
+        "dayEvents": [
+        {"name":"Journée et soirée jeux à Saint Genis Pouilly, salle Jean Monnet",
+          "hour":"09:00"}
+        ]
+      },
+      "2015-01-25": {
+        "number" : 1,
+        "badgeClass":"badge-warning", 
+        "dayEvents": [
+        {"name":"Journée et soirée jeux à Saint Genis Pouilly, salle Jean Monnet",
+          "hour":"09:00"}
+        ]
+      },
+      "2015-02-07": {},
+      "2015-02-21": {},
+      "2015-03-07": {},
+      "2015-03-21": {},
+      "2015-04-11": {},
+      "2015-04-25": {},
+      "2015-05-09": {},
+      "2015-05-20": {
+        "number" : 1,
+        "badgeClass":"badge-warning", 
+        "dayEvents": [
+        {"name":"Assemblée générale",
+          "hour":"09:00"}
+        ]
+      },
+      "2015-05-30": {},
+      "2015-06-13": {},
+      "2015-06-27": {
+        "number" : 1,
+        "badgeClass":"badge-warning", 
+        "dayEvents": [
+        {"name":"journée continue",
+          "hour":"09:00"}
+        ]
+      },
+      "2015-07-04": {
+        "number" : 1,
+        "badgeClass":"badge-warning", 
+        "dayEvents": [
+        {"name":"journée inventaire",
+          "hour":"09:00"}
+        ]
+      }
     }
   });
 };
